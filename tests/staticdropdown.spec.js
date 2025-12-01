c6a216eb47360d84aa09591f4e417d1c7574b0ab
ningappamn <ningappa.mn14@gmail.com>
2025-12-01 20:24:50 +0530

    staticdropdown

---
+++ tests/staticdropdown.spec.js
@@ -0,0 +1,16 @@
+import{test,expect} from '@playwright/test';
+
+test('static dropdown',async({page})=>{
+
+    await page.goto('https://testautomationpractice.blogspot.com/');
+    const clickdropdown=await page.locator('#country').click()
+    const selectopt= await page.locator('#country').selectOption('germany');
+    await page.waitForTimeout(5000);
+    expect(page.locator('#country')).toHaveValue('germany');
+
+    
+
+   // await page.pause();
+    
+
+})
