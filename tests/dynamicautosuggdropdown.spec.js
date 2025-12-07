1f2a4ffdb6e8ed364a6de319f486f06949f4320a
ningappamn <ningappa.mn14@gmail.com>
2025-12-07 12:40:50 +0530

    removed prabhu and dynamic dropdown file

--- tests/dynamicautosuggdropdown.spec.js
+++
@@ -1,9 +0,0 @@
-import{test,expect} from '@playwright/test';
-
-test('handle auto suggested dropdown',async({page})=>{
-
-await page.goto('https://www.amazon.in/');
-const searchtext=await page.getByLabel('Search Amazon.in').fill('laptop');
-page.locator('#nav-flyout-iss-anchor div');
-
-})
